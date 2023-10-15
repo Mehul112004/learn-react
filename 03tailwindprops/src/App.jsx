200e9c51974cb6a940bc9d90d0973e7464f40b97
Mehul <mehulbohra11@gmail.com>
2023-10-15 22:58:44 +0530

    third asset

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from'./Card.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 p-4  rounded-2xl'>Hello world</h1>
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ 03tailwindprops/src/Card.jsx
@@ -0,0 +1,8 @@
+function Card({id="hh"}){
+    return (
+        <>
+            <h1 className="border-2 mt-4">This is a card {id}</h1>
+        </>
+    )
+}
+export default Card
